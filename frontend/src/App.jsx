bb16f57828051c802b863f276fb513dbcec65da4
Stormlite12 <sid2002official@gmail.com>
2025-09-26 09:58:14 +0530

    EDIT: fixed some typos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import Home from './pages/home'
 import AdminPanel from './components/AdminPanel'
 import AuthModal from './components/AuthModal'
 import CartSidebar from './components/CartSidebar'
